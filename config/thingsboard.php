0eb73bf2d307191b3dbce10e4ba5a3f67bed2bb7
jalallinux <jalallinux@users.noreply.github.com>
2023-05-21 06:39:04 +0000

    Fix styling

--- config/thingsboard.php
+++ config/thingsboard.php
@@ -14,9 +14,9 @@ return [
         'admin' => [
             'mail' => env('THINGSBOARD_ADMIN_MAIL', 'sysadmin@thingsboard.org'),
             'pass' => env('THINGSBOARD_ADMIN_PASS', 'sysadmin'),
-//            'user_id' => env('THINGSBOARD_ADMIN_USER_ID', 'cf8f9240-ed9b-11ed-9c52-f37143f58f87'),
-//            'tenant_id' => env('THINGSBOARD_ADMIN_TENANT_ID', '13814000-1dd2-11b2-8080-808080808080'),
-//            'customer_id' => env('THINGSBOARD_ADMIN_CUSTOMER_ID', '13814000-1dd2-11b2-8080-808080808080'),
+            //            'user_id' => env('THINGSBOARD_ADMIN_USER_ID', 'cf8f9240-ed9b-11ed-9c52-f37143f58f87'),
+            //            'tenant_id' => env('THINGSBOARD_ADMIN_TENANT_ID', '13814000-1dd2-11b2-8080-808080808080'),
+            //            'customer_id' => env('THINGSBOARD_ADMIN_CUSTOMER_ID', '13814000-1dd2-11b2-8080-808080808080'),
         ],
     ],
 ];
