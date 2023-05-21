0c645ae49eee28016e091b8a546b6fce3ee060a4
jalallinux <jalallinux@users.noreply.github.com>
2023-05-21 08:39:22 +0000

    Fix styling

--- src/Interfaces/ThingsboardUser.php
+++ src/Interfaces/ThingsboardUser.php
@@ -5,5 +5,6 @@ namespace JalalLinuX\Tntity\Interfaces;
 interface ThingsboardUser
 {
     public function getThingsboardEmailAttribute(): string;
+
     public function getThingsboardPasswordAttribute(): string;
 }
