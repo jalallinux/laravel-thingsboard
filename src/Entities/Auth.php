814e2c3cf0e8f4d7ea6b1854c564e78e25012179
JalalLinuX <smjjalalzadeh93@gmail.com>
2023-05-22 15:56:08 +0330

    add Auth@changePAssword

--- src/Entities/Auth.php
+++ src/Entities/Auth.php
@@ -19,4 +19,11 @@ class Auth extends Tntity
             $this->api(true)->get('auth/user')->json()
         );
     }
+
+    public function changePassword(string $current, $new): bool
+    {
+        return $this->api()->post('auth/changePassword', [
+            'currentPassword' => $current, 'newPassword' => $new,
+        ])->successful();
+    }
 }
