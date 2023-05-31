73bea23806453b788bad4e6e50f878f07f442233
SamirSabiee <samirsabiee@gmail.com>
2023-05-31 13:06:48 +0330

    add saveEntityAttributesV2 methods with unit test

--- src/Entities/Telemetry.php
+++ src/Entities/Telemetry.php
@@ -38,9 +38,9 @@ class Telemetry extends Tntity
      * }
      * }
      *
-     * @param  array  $payload
-     * @param  EnumTelemetryScope  $scope
-     * @param  string  $deviceId
+     * @param array $payload
+     * @param EnumTelemetryScope $scope
+     * @param string $deviceId
      * @return bool
      *
      * @throws \Throwable
@@ -61,7 +61,7 @@ class Telemetry extends Tntity
         }
 
         throw_if(
-            ! Str::isUuid($deviceId),
+            !Str::isUuid($deviceId),
             $this->exception('method "deviceId" argument must be a valid uuid.'),
         );
 
@@ -72,9 +72,9 @@ class Telemetry extends Tntity
      * Delete device attributes using provided Device Id, scope and a list of keys.
      * Referencing a non-existing Device Id will cause an error
      *
-     * @param  EnumTelemetryScope  $scope
-     * @param  array  $keys
-     * @param  string  $deviceId
+     * @param EnumTelemetryScope $scope
+     * @param array $keys
+     * @param string $deviceId
      * @return bool
      *
      * @throws \Throwable
@@ -90,7 +90,7 @@ class Telemetry extends Tntity
         }
 
         throw_if(
-            ! Str::isUuid($deviceId),
+            !Str::isUuid($deviceId),
             $this->exception('method "deviceId" argument must be a valid uuid.'),
         );
 
@@ -121,17 +121,17 @@ class Telemetry extends Tntity
      * }
      * Referencing a non-existing entity Id or invalid entity type will cause an error.
      *
+     * @param array $payload
+     * @param EnumEntityType $entityType
+     * @param EnumTelemetryScope $scope
+     * @param string $entityId
+     * @return bool
+     *
+     * @throws \Throwable
      * @author Sabiee
      *
      * @group TENANT_ADMIN | CUSTOMER_USER
      *
-     * @param  array  $payload
-     * @param  EnumEntityType  $entityType
-     * @param  EnumTelemetryScope  $scope
-     * @param  string  $entityId
-     * @return bool
-     *
-     * @throws \Throwable
      */
     public function saveEntityAttributesV1(array $payload, EnumEntityType $entityType, string $entityId, EnumTelemetryScope $scope): bool
     {
@@ -145,7 +145,7 @@ class Telemetry extends Tntity
         );
 
         throw_if(
-            ! Str::isUuid($entityId),
+            !Str::isUuid($entityId),
             $this->exception('method "entityId" argument must be a valid uuid.'),
         );
 
@@ -167,7 +167,7 @@ class Telemetry extends Tntity
         }
 
         throw_if(
-            ! Str::isUuid($entityId),
+            !Str::isUuid($entityId),
             $this->exception('method "entityId" argument must be a valid uuid.'),
         );
 
@@ -175,5 +175,61 @@ class Telemetry extends Tntity
 
         return $this->api(self::config('rest.exception.throw_bool_methods'))->bodyFormat('query')
             ->delete("plugins/telemetry/{$entityType}/{$entityId}/{$scope}", ['keys' => $keys])->successful();
+    }
+
+    /**
+     * Creates or updates the entity attributes based on Entity Id and the specified attribute scope.
+     * List of possible attribute scopes depends on the entity type:
+     *
+     * SERVER_SCOPE - supported for all entity types;
+     * SHARED_SCOPE - supported for devices.
+     * The request payload is a JSON object with key-value format of attributes to create or update. For example:
+     *
+     * {
+     * "stringKey":"value1",
+     * "booleanKey":true,
+     * "doubleKey":42.0,
+     * "longKey":73,
+     * "jsonKey": {
+     * "someNumber": 42,
+     * "someArray": [1,2,3],
+     * "someNestedObject": {"key": "value"}
+     * }
+     * }
+     * @param array $payload
+     *
+     * @param EnumEntityType $entityType
+     *
+     * @param string $entityId
+     *
+     * @param EnumTelemetryScope $scope
+     *
+     * @return bool
+     *
+     * @throws \Throwable
+     *
+     * @author Sabiee
+     *
+     * @group TENANT_ADMIN | CUSTOMER_USER
+     */
+    public function saveEntityAttributesV2(array $payload, EnumEntityType $entityType, string $entityId, EnumTelemetryScope $scope): bool
+    {
+
+        if (empty($payload)) {
+            throw $this->exception('method "payload" cannot be empty');
+        }
+
+        throw_if(
+            !Str::isUuid($entityId),
+            $this->exception('method "entityId" argument must be a valid uuid.'),
+        );
+
+        throw_if(
+            $scope === EnumTelemetryScope::CLIENT_SCOPE(),
+            $this->exception('method "scope" can\'t be client scope'),
+        );
+
+        return $this->api(self::config('rest.exception.throw_bool_methods'))->post("plugins/telemetry/{$entityType}/{$entityId}/attributes/{$scope}", $payload)->successful();
+
     }
 }
---
+++ tests/Unit/Telemetry/SaveEntityAttributesV2Test.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace JalalLinuX\Thingsboard\Tests\Unit\Telemetry;
+
+use JalalLinuX\Thingsboard\Enums\EnumAuthority;
+use JalalLinuX\Thingsboard\Enums\EnumEntityType;
+use JalalLinuX\Thingsboard\Enums\EnumTelemetryScope;
+use JalalLinuX\Thingsboard\Infrastructure\PaginationArguments;
+use JalalLinuX\Thingsboard\Tests\TestCase;
+
+class SaveEntityAttributesV2Test extends TestCase
+{
+    /**
+     * @throws \Throwable
+     */
+    public function testCorrectPayload()
+    {
+        $tenantUser = $this->thingsboardUser(EnumAuthority::TENANT_ADMIN());
+        $deviceId = thingsboard($tenantUser)->device()->getTenantDeviceInfos(PaginationArguments::make())->data()->first()->id->id;
+        $scope = $this->faker->randomElement(array_diff(EnumTelemetryScope::cases(), [EnumTelemetryScope::CLIENT_SCOPE()]));
+        $key = $this->faker->word();
+        $value = $this->faker->word();
+        $result = thingsboard($tenantUser)->telemetry()->saveEntityAttributesV2([$key => $value], EnumEntityType::DEVICE(), $deviceId, $scope);
+        thingsboard($tenantUser)->telemetry()->deleteEntityAttributes(EnumEntityType::DEVICE(), $deviceId, $scope, [$key]);
+        $this->assertTrue($result);
+    }
+
+    /**
+     * @throws \Throwable
+     */
+    public function testInvalidPayload()
+    {
+        $tenantUser = $this->thingsboardUser(EnumAuthority::TENANT_ADMIN());
+        $this->expectExceptionCode(500);
+        $this->expectExceptionMessageMatches('/payload/');
+        thingsboard($tenantUser)->telemetry()->saveEntityAttributesV2([], EnumEntityType::DEVICE(), $this->faker->uuid, EnumTelemetryScope::SERVER_SCOPE());
+    }
+
+    /**
+     * @throws \Throwable
+     */
+    public function testInvalidScope()
+    {
+        $tenantUser = $this->thingsboardUser(EnumAuthority::TENANT_ADMIN());
+        $this->expectExceptionCode(500);
+        $this->expectExceptionMessageMatches('/scope/');
+        thingsboard($tenantUser)->telemetry()->saveEntityAttributesV2(['a' => 'b'], EnumEntityType::DEVICE(), $this->faker->uuid, EnumTelemetryScope::CLIENT_SCOPE());
+    }
+
+    /**
+     * @throws \Throwable
+     */
+    public function testInvalidUuid()
+    {
+        $tenantUser = $this->thingsboardUser(EnumAuthority::TENANT_ADMIN());
+        $this->expectExceptionCode(500);
+        $this->expectExceptionMessageMatches('/entityId/');
+        thingsboard($tenantUser)->telemetry()->saveEntityAttributesV2(['a' => 'b'], EnumEntityType::DEVICE(), substr_replace($this->faker->uuid, 'z', -1), EnumTelemetryScope::SHARED_SCOPE());
+    }
+}
