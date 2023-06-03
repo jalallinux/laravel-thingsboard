c40c9c154163302d98cbb86b773bc8c908e9f9e5
JalalLinuX <smjjalalzadeh93@gmail.com>
2023-06-03 16:57:14 +0330

    work on mqtt test

--- tests/Unit/Mqtt/TelemetryTest.php
+++ tests/Unit/Mqtt/TelemetryTest.php
@@ -4,7 +4,7 @@ namespace JalalLinuX\Thingsboard\Tests\Unit\Mqtt;
 
 use JalalLinuX\Thingsboard\Tests\TestCase;
 
-class TelemetryUploadTest extends TestCase
+class TelemetryTest extends TestCase
 {
     public function testSuccess()
     {
@@ -12,7 +12,7 @@ class TelemetryUploadTest extends TestCase
             'A1_TEST_TOKEN', 'A2_TEST_TOKEN', 'A3_TEST_TOKEN', 'B1_TEST_TOKEN', 'C1_TEST_TOKEN',
         ]);
 
-        thingsboard()->mqtt('A1_TEST_TOKEN')->telemetry([
+        thingsboard()->mqtt($deviceToken)->telemetry([
             ['ts' => $this->faker->dateTimeBetween('-5 years')->getTimestamp() * 1000, 'values' => ['test-temperature' => $this->faker->numerify('##')]],
             ['ts' => $this->faker->dateTimeBetween('-5 years')->getTimestamp() * 1000, 'values' => ['test-humidity' => $this->faker->numerify('##')]],
         ]);
