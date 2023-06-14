40898f88fe88a8c68406b2ca5e8733d2a5f2894f
JalalLinuX <smjjalalzadeh93@gmail.com>
2023-06-14 10:50:06 +0330

    fix event date validation
    Co-authored-by: SamirSabiee <samirsabiee@gmail.com>
    Co-authored-by: Ehsan Maiqani <spin313@gmail.com>

--- lang/en/exception.php
+++ lang/en/exception.php
@@ -4,5 +4,4 @@ return [
     'default' => 'Something went wrong !',
     'with_token' => 'Method need authentication token.',
     'default_descriptor_undefined' => 'Default widget type descriptor :descriptor undefined in config file.',
-    'start_bigger_then_end' => 'start time can\'t be bigger than end time',
 ];
--- lang/en/validation.php
+++ lang/en/validation.php
@@ -9,5 +9,8 @@ return [
     'assoc_array' => 'The :attribute must be an associative array.',
     'assoc_array_of' => 'The :attribute must be an associative array of :struct.',
     'in' => 'The attribute :attribute must be one of these values:.',
-
+    'after' => 'The :attribute must be a date after :date.',
+    'after_or_equal' => 'The :attribute must be a date after or equal to :date.',
+    'before' => 'The :attribute must be a date before :date.',
+    'before_or_equal' => 'The :attribute must be a date before or equal to :date.',
 ];
