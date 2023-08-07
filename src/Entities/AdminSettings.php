d9b49a3821a2f92fbd69301f2e772b78c8ceb53b
JalalLinuX <smjjalalzadeh93@gmail.com>
2023-08-07 13:14:39 +0330

    complete saveAdminSettings

--- src/Entities/AdminSettings.php
+++ src/Entities/AdminSettings.php
@@ -54,4 +54,31 @@ class AdminSettings extends Tntity
 
         return $this->fill($adminSettings);
     }
+
+    /**
+     * Creates or Updates the Administration Settings.
+     * Platform generates random Administration Settings Id during settings creation.
+     * The Administration Settings Id will be present in the response.
+     * Specify the Administration Settings Id when you would like to update the Administration Settings.
+     * Referencing non-existing Administration Settings Id will cause an error.
+     *
+     * @param  string  $key
+     * @param  array  $values
+     * @return $this
+     *
+     * @author JalalLinuX
+     *
+     * @group SYS_ADMIN
+     */
+    public function saveAdminSettings(string $key, array $values): static
+    {
+        $payload = array_merge($this->attributesToArray(), [
+            'key' => $key,
+            'jsonValue' => $values,
+        ]);
+
+        $adminSettings = $this->api()->post('admin/settings', $payload)->json();
+
+        return $this->fill($adminSettings);
+    }
 }
